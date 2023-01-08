df7a697a95623435e4ae558d6fbbad5df4c091c4
Gülay DEMİR <120813142+gulaydemiir0@users.noreply.github.com>
2023-01-08 16:52:19 +0300

    Update Process.java

--- src/dispatcher/Process.java
+++ src/dispatcher/Process.java
@@ -8,8 +8,14 @@ public class Process {
     private int priority;
     private int burstTime;
     public int startingBurstTime;
-
-    public Process(int arrivalTime, int priority, int burstTime) {
+	
+// 'int id' proses idsini tutan değişken
+// 'int arrivalTime' varış zamanını tutan değişken
+// 'int priority' öncelik düzeyini tutan değişken
+// 'int burstTime' patlama zamanını tutan değişken
+// 'int startingBurstTime' başlangıçtaki patlama süresini tutan değişken
+   
+	public Process(int arrivalTime, int priority, int burstTime) {
         this.arrivalTime = arrivalTime;
         this.priority = priority;
         this.burstTime = burstTime;
